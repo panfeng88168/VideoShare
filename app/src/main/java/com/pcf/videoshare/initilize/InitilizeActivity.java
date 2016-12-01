b269fca40ba8ae34a60bc0e0ad2e5db50d6e2ca1
Test <panfeng88168@163.com>
2016-12-01 16:30:26 +0800

    第一版本

--- app/src/main/java/com/pcf/videoshare/initilize/InitilizeActivity.java
+++ app/src/main/java/com/pcf/videoshare/initilize/InitilizeActivity.java
@@ -3,8 +3,11 @@ package com.pcf.videoshare.initilize;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.pcf.videoshare.R;
+
+import org.w3c.dom.Text;
 
 public class InitilizeActivity extends Activity {
 
@@ -12,6 +15,6 @@ public class InitilizeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //TODO asdfasdfasdf
-
+        TextView tv=findViewById(R.id.tv);
     }
 }
