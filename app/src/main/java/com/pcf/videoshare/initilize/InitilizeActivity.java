1c37adeb791629d24272224c0a836a9bc0453229
Test <panfeng88168@163.com>
2016-12-01 16:18:14 +0800

    Initial commit

---
+++ app/src/main/java/com/pcf/videoshare/initilize/InitilizeActivity.java
@@ -0,0 +1,17 @@
+package com.pcf.videoshare.initilize;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pcf.videoshare.R;
+
+public class InitilizeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //TODO asdfasdfasdf
+
+    }
+}
