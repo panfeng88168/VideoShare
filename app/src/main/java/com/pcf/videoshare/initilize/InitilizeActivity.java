a219881fbcd9764213ad42090ef544ed22dd0485
panfeng88168 <panfeng88168@gmail.com>
2016-12-05 13:35:34 +0800

    第三版本2

--- app/src/main/java/com/pcf/videoshare/initilize/InitilizeActivity.java
+++ app/src/main/java/com/pcf/videoshare/initilize/InitilizeActivity.java
@@ -8,11 +8,21 @@ import com.pcf.videoshare.R;
 
 public class InitilizeActivity extends Activity {
 
+    private String mInt;
+
+    public String getmInt() {
+        return mInt;
+    }
+
+    public void setmInt(String mInt) {
+        this.mInt = mInt;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //TODO asdfasdfasdf
         TextView tv= (TextView) findViewById(R.id.tv);
-        tv.setText("test!!!");
+        tv.setText("TEST!!!");
     }
 }
